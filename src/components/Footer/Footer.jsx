c513af4fc7a134b8a5cf57ecbf73a48bd37f075c
Алекс Южный <youzhny.foto@gmail.com>
2023-10-17 12:45:55 +0300

    add footer and sidebar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,47 @@
+import styles from '../../styles/Footer.module.css';
+
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '../../utils/routes';
+import LOGO from '../../images/logo.svg';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <section className={styles.footer}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt='logo' />
+                </Link>
+            </div>
+            <div className={styles.rights}>
+                Developed by{' '}
+                <a href='https://github.com/AlexPirko/' target='_blank' rel='noreferrer'>
+                    Olexandr Pirko
+                </a>
+            </div>
+
+            <div className={styles.socials}>
+                <a href='https://instagram.com' target='_blank' rel='noreferrer'>
+                    <svg className='icon'>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+                    </svg>
+                </a>
+
+                <a href='https://facebook.com' target='_blank' rel='noreferrer'>
+                    <svg className='icon'>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+                    </svg>
+                </a>
+
+                <a href='https://youtube.com' target='_blank' rel='noreferrer'>
+                    <svg className='icon'>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+                    </svg>
+                </a>
+            </div>
+        </section>
+    );
 };
 
 export default Footer;
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,31 @@
+import styles from '../../styles/Sidebar.module.css';
+import cl from 'classnames';
+
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SideBar = () => {
-    return <div>SideBar</div>;
+    return (
+        <section className={styles.sidebar}>
+            <div className={styles.title}>CATEGORIES</div>
+            <nav>
+                <ul className={styles.menu}>
+                    <li>
+                        <NavLink to={`/categories/${1}`}>Link</NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className={styles.footer}>
+                <a href='/help' target='_blank' className={styles.link}>
+                    Help
+                </a>
+                <a href='/terms' target='_blank' className={cl(styles.link, styles.link_terms)}>
+                    Terms & Conditions
+                </a>
+            </div>
+        </section>
+    );
 };
 
 export default SideBar;
