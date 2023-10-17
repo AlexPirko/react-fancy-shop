1db62202b79360e1957f0acb1c70ae6708be1332
Алекс Южный <youzhny.foto@gmail.com>
2023-10-17 21:54:55 +0300

    add filtered gallery

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,32 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import Poster from '../Poster/Poster';
 import Products from '../Products/Products';
 import Categories from '../Categories/Categories';
 import Banner from '../Banner/Banner';
+import { filterByPrice } from '../../features/products/productsSlice';
 
 const Home = () => {
-    const { products, categories } = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const {
+        products: { list, filtered },
+        categories,
+    } = useSelector((state) => state);
+
+    useEffect(() => {
+        if (!list.length) return;
+
+        dispatch(filterByPrice(100));
+    }, [dispatch, list.length]);
 
     return (
         <>
             <Poster />
-            <Products products={products.list} amount={5} title='Favorites' />
+            <Products products={list} amount={5} title='Favorites' />
             <Categories products={categories.list} amount={5} title='Choose category' />
             <Banner />
+            <Products products={filtered} amount={5} title='Less then 100$' />
         </>
     );
 };
